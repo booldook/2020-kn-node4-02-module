563ca5515c6cc25d3c67a3a5436c816a4bab8f61
booldook <booldook@gmail.com>
2020-10-20 22:20:09 +0900

    ...

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const path = require('path');
 const express = require('express');
-const { nowDateIso, nowDateKorean } = require('./modules/sample');
-console.log(  nowDateIso() );
-console.log(  nowDateKorean()  );
+const { moment, nowDateIso, nowDateKorean } = require('./modules/date');
+// console.log(  nowDateIso() );
+// console.log(  nowDateKorean()  );
+// console.log(  moment );
+
+const notFound = path.join(__dirname, './public/404.html');
+
+const app = express();
+app.listen(3000, () => { console.log('http://127.0.0.1:3000'); });
+
+app.use('/', express.static(path.join(__dirname, './public')));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello</h1>');
+});
+
+app.use((req, res) => {
+  res.sendFile(notFound);
+  //res.redirect('/404.html');
+});
